48bd87f46dea60d9bab13dd14108a05731b5e438
buihai.nb.gv <buihai.nb.gv@gmail.com>
2012-04-18 03:03:56 +0000

    generate document javadoc

--- src/com/pit/list/SortedListModel.java
+++ src/com/pit/list/SortedListModel.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pit.list;
 
 import java.util.Arrays;
@@ -12,7 +8,7 @@ import java.util.TreeSet;
 import javax.swing.AbstractListModel;
 
 /**
- *
+ * Action (Add, Remove) JList on Interface
  * @author Administrator
  */
 public class SortedListModel extends AbstractListModel {
