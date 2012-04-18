2c14f1a08e29a6e4e7eb84e223ee118261c44773
buihai.nb.gv <buihai.nb.gv@gmail.com>
2012-04-18 02:01:22 +0000

    edit code checkPSCD and logPSCD

--- src/com/pit/conn/ConnectDB.java
+++ src/com/pit/conn/ConnectDB.java
@@ -33,7 +33,7 @@ public class ConnectDB {
             String password = config_ora[2];
             conn = DriverManager.getConnection(url, username, password);
         } catch (SQLException ex) {
-            throw new SQLException(ex.getMessage());            
+            throw new SQLException(ex.getMessage());
         } catch (ClassNotFoundException cnfe) {
             System.err.println("Driver not found.");
         } catch (Exception e) {
@@ -73,7 +73,7 @@ public class ConnectDB {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rset = null;
-        String sql = "insert into tb_cqt_convert (short_name, type_data, imp_date) values ('"+short_name+"','"+type_date+"','"+imp_time+"')";
+        String sql = "insert into tb_cqt_convert (short_name, type_data, imp_date) values ('" + short_name + "','" + type_date + "','" + imp_time + "')";
         try {
             conn = ConvertPSCDVATApp.connORA;
             stmt = conn.createStatement();
@@ -86,7 +86,7 @@ public class ConnectDB {
 //            conn.close();
         }
     }
-    
+
     /**
      * @desc delete data of file excel
      * @param sql
@@ -104,9 +104,9 @@ public class ConnectDB {
 
         }
         //Query delete table TB_NO, TB_PS, TB_TK
-        String sql_no = "delete from tb_no a where short_name = '"+try_cqt+"' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
-        String sql_ps = "delete from tb_ps a where short_name = '"+try_cqt+"' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
-        String sql_tk = "delete from tb_tk a where short_name = '"+try_cqt+"' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
+        String sql_no = "delete from tb_no a where short_name = '" + try_cqt + "' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
+        String sql_ps = "delete from tb_ps a where short_name = '" + try_cqt + "' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
+        String sql_tk = "delete from tb_tk a where short_name = '" + try_cqt + "' and a.imp_file in (" + del_file.substring(0, del_file.length() - 1) + ")";
 
         Connection conn = null;
         Statement stmt = null;
@@ -159,7 +159,6 @@ public class ConnectDB {
         }
     }
 
-    
     /**
      * 
      * @param sql
@@ -204,7 +203,7 @@ public class ConnectDB {
             stmt = conn.createStatement();
             rset = stmt.executeQuery(sql);
             while (rset.next()) {
-                info = rset.getString("tax_code") + "," + rset.getString("short_name") +","+rset.getString("tax_model");
+                info = rset.getString("tax_code") + "," + rset.getString("short_name") + "," + rset.getString("tax_model");
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -223,20 +222,20 @@ public class ConnectDB {
      * @return number convert
      */
     public static int[] getNumCV(String short_name) throws SQLException {
-        int cqt_cv [] = new int[3];              
+        int cqt_cv[] = new int[3];
         Connection conn = null;
-        Statement stmt  = null;
-        ResultSet rset  = null;
+        Statement stmt = null;
+        ResultSet rset = null;
         try {
             conn = ConvertPSCDVATApp.connORA;
             stmt = conn.createStatement();
             String sql = "SELECT count(type_data) num_convert FROM tb_cqt_convert a where a.short_name = '" + short_name
-                       + "' and  a.type_data in ('NO','PS','TK') group by type_data order by a.type_data";
+                    + "' and  a.type_data in ('NO','PS','TK') group by type_data order by a.type_data";
             rset = stmt.executeQuery(sql);
             int i = 0;
             while (rset.next()) {
                 cqt_cv[i] = rset.getInt("num_convert");
-                i ++;
+                i++;
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -247,30 +246,180 @@ public class ConnectDB {
         }
         return cqt_cv;
     }
-    
-    
+
     /*
      * @desc insert error code into database
      * @param sql
      * @throws SQLException
      */
-    public static void insErrCode(String short_name, String rid, String err_string) throws SQLException {
+    public static void insUnSplitErrCode(String short_name, String rid, String table_name, String err_string) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql = "insert into tb_unsplit_data_error (short_name, rid, table_name, err_string)"
+                + " values ( '" + short_name + "',"
+                + " '" + rid + "',"
+                + " '" + table_name + "',"
+                + " '" + err_string + "')";
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    /**
+     * @desc xóa dữ liệu trong bảng tb_unsplit_data_error khi thực hiện kiểm tra lại
+     * @param short_name
+     * @param table_name
+     * @throws SQLException 
+     */
+    public static void delUnSplitErrCode(String short_name, String table_name) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql = "delete from tb_unsplit_data_error"
+                + " where short_name = '" + short_name + "'"
+                + " and table_name = '" + table_name + "'";
+
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    /**
+     * @desc xóa dữ liệu cũ trong bảng tb_data_error khi thực hiện kiểm tra lại
+     * @param short_name
+     * @param table_name
+     * @throws SQLException 
+     */
+    public static void delDataErrCode(String short_name, String table_name) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql = "delete from tb_data_error"
+                + " where short_name = '" + short_name + "'"
+                + " and table_name = '" + table_name + "'";
+
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    /**
+     * @desc gọi tới thủ tục kiểm tra dữ liệu Oracle
+     * @param prcName
+     * @param short_name
+     * @throws SQLException 
+     */
+    public static void callOraclePrcChk(String prcName, String short_name) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql;
+
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+
+            sql = "begin pck_check_data." + prcName + "('" + short_name + "', '" + loadNgayChotDL(short_name) + "'); end;";
+//                System.out.println(sql);s
+            stmt.execute(sql);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    /**
+     * @desc Insert log
+     * @param short_name
+     * @param pck
+     * @throws SQLException 
+     */
+    public static void callOraclePrc_Ins_Log_Vs(String short_name, String pck, String status, String mesg) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql;
+
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+
+            sql = "begin pck_trace_log.Prc_Ins_Log_Vs('" + short_name + "', '" + pck + "', '" + status + "', '" + mesg + "'); end;";
+//                System.out.println(sql);
+            stmt.execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    /**
+     * 
+     * @param fncName
+     * @param tax
+     * @param tb_name
+     * @throws SQLException 
+     */
+    public static void callOraclePrcInsSplitErr(String prcName, String short_name, String table_name) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        String sql;
+
+        try {
+            conn = ConvertPSCDVATApp.connORA;
+            stmt = conn.createStatement();
+
+            sql = "begin pck_check_data." + prcName + "('" + short_name + "', '" + table_name + "'); end;";
+//                System.out.println(sql);
+            stmt.execute(sql);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            stmt.close();
+        }
+    }
+
+    public static String loadNgayChotDL(String short_name) throws SQLException {
+        //Ngày chốt
+        String ngay_chot = "";
+
         Connection conn = null;
         Statement stmt = null;
         ResultSet rset = null;
-        String sql = "insert into tb_unsplit_data_error (short_name, rid, table_name, err_string) values ("
-                + short_name + ", '" + rid + "', 'TB_TK', '" + err_string + "')";
         try {
-            System.out.println(sql);
             conn = ConvertPSCDVATApp.connORA;
             stmt = conn.createStatement();
-            rset = stmt.executeQuery(sql);
+            rset = stmt.executeQuery("SELECT a.short_name, to_char(a.ky_no_den, 'DD-MON-YYYY') as ky_no_den FROM tb_lst_taxo a where a.short_name = '" + short_name + "'");
+            while (rset.next()) {
+                ngay_chot = rset.getString("ky_no_den");
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
         } finally {
             rset.close();
             stmt.close();
+//            conn.close();
         }
+        return ngay_chot;
     }
-
 }
