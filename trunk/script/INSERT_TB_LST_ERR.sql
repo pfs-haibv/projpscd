525c3130cf9a5448bb0aaeb60fc5ab5e0e43fa50
buihai.nb.gv@gmail.com <buihai.nb.gv@gmail.com@990be4bd-e079-5f22-5fe9-d67c79ac5753>
2013-10-23 07:29:51 +0000

    update tb_lst_err

--- trunk/script/INSERT_TB_LST_ERR.sql
+++ trunk/script/INSERT_TB_LST_ERR.sql
@@ -1,42 +1,105 @@
-INSERT INTO tb_lst_err 
-VALUES('020','NNT la ca nhan nh?ng t?n t?i t? khai PNN 02/TK-SDDPN danh cho t? ch?c',NULL);
-INSERT INTO tb_lst_err 
-VALUES('021','T?n t?i t? khai tren Header nh?ng khong co thong tin tren Detail',NULL);
-INSERT INTO tb_lst_err 
-VALUES('001','Lo?i giao d?ch khong h?p l?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('002','M? c? quan thu? khong t?n t?i trong danh m?c',NULL);
-INSERT INTO tb_lst_err 
-VALUES('003','M? Tin khong t?n t?i trong h? th?ng dang ky thu?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('004','M? TIN khong phu h?p ',NULL);
-INSERT INTO tb_lst_err 
-VALUES('005','M? ch??ng khong t?n t?i trong danh m?c',NULL);
-INSERT INTO tb_lst_err 
-VALUES('006','M? ti?u m?c khong t?n t?i trong danh m?c',NULL);
-INSERT INTO tb_lst_err 
-VALUES('007','Ti?u m?c khong dung v?i m? t? khai',NULL);
-INSERT INTO tb_lst_err 
-VALUES('008','Tai kho?n khong h?p l?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('009','D? li?u sai d?nh d?ng',NULL);
-INSERT INTO tb_lst_err 
-VALUES('010','M? t? khai khong h?p l?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('011','K? tinh thu? khong h?p l?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('012','Ngay khong h?p l?',NULL);
-INSERT INTO tb_lst_err 
-VALUES('013','Thi?u tr??ng b?t bu?c ',NULL);
-INSERT INTO tb_lst_err 
-VALUES('014','Sai l?i s? h?c',NULL);
-INSERT INTO tb_lst_err 
-VALUES('015','CA, CO khong phu h?p',NULL);
-INSERT INTO tb_lst_err 
-VALUES('016','Khong t?n t?i trong danh m?c map',NULL);
-INSERT INTO tb_lst_err 
-VALUES('017','T?n t?i d? li?u con ph?i n?p, n?p th?a v?i NNT lam cong an luong',NULL);
-INSERT INTO tb_lst_err 
-VALUES('018','NNT co ph??ng phap tinh thu? khac ph??ng phap khoan, nh?ng t?n t?i can c? tinh thu? GTGT',NULL);
-INSERT INTO tb_lst_err 
-VALUES('019','NNT la t? ch?c nh?ng t?n t?i t? khai PNN 01/TK-SDDPNN danh cho ca nhan, h? gia dinh',NULL);
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('001','Lo¹i giao dÞch kh«ng hîp lÖ Lo¹i giao dÞch kh¸c TK, CD',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('002','M· c¬ quan thuÕ kh«ng tån t¹i trong danh môc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('003','M· Tin kh«ng tån t¹i trong hÖ thèng ®¨ng ký thuÕ',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('004','M· TIN kh«ng phï hîp',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('005','M· ch­¬ng kh«ng tån t¹i trong danh môc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('006','M· tiÓu môc kh«ng tån t¹i trong danh môc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('007','TiÓu môc kh«ng ®óng víi m· tê khai',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('008','Tµi kho¶n kh«ng hîp lÖ',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('009','D÷ liÖu sai ®Þnh d¹ng - C¸c tr­êng ngµy th¸ng kh¸c ®Þnh d¹ng DD/MM/YYYY',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('010','M· tê khai kh«ng hîp lÖ',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('011','Kú t×nh thuÕ kh«ng hîp lÖ',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('012','Ngµy kh«ng phï hîp',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('013','ThiÕu tr­êng b¾t buéc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('014','Sai lçi sè häc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('015','CA, CO kh«ng phï hîp',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('016','Kh«ng tån t¹i trong danh muc map',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('017','Tån t¹i d÷ liÖu cßn ph¶i nép, nép thõa víi NNT lµm c«ng ¨n l­¬ng',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('018','NNT cã ph­¬ng ph¸p t×nh thuÕ kh¸c ph­¬ng ph¸p kho¸n, nh­ng tån t¹i c¨n cø tÝnh thuÕ GTGT',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('019','NNT lµ tæ chøc nh­ng tån t¹i tê khai PNN 01/TK-SDDPNN dµnh cho c¸ nh©n, hé gia ®×nh',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('020','NNT lµ c¸ nh©n nh­ng tån t¹i tê khai PNN 02/TK-SDDPN dµnh cho tæ chøc',NULL)
+/
+INSERT INTO tb_lst_err
+("ERR_ID","ERR_NAME","ERR_TYPE")
+VALUES
+('021','Tån t¹i tê khai trªn Header nh­ng kh«ng cã th«ng tin trªn Detail',NULL)
+/
